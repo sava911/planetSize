cce41e4b8a8d2871a45b4659cde6fde0ac92e4f9
sava911 <pesss929@gmail.com>
2023-08-31 20:08:49 +0300

    Updates

---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+let range = document.querySelector('.range')
+let divider = document.querySelector('body')
+
+range.addEventListener('input', () => {
+  let out = Math.abs(range.value)
+  divider.style.setProperty('--divider', out)
+})
